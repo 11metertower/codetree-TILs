14c233973ab445d49768ffec7c8a0286308f6138
11metertower <87414569+11metertower@users.noreply.github.com>
2024-10-04 18:50:44 +0900

    Title: 줄 겹치게 접기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 241004/줄 겹치게 접기/rope-folding.cpp
+++ 241004/줄 겹치게 접기/rope-folding.cpp
@@ -2,45 +2,60 @@
 #include <algorithm>
 using namespace std;
 int point[10002];
+
+int curr=1, minp, maxp, n;
+
+int fold(float i)
+{
+    int flag, p1, p2;
+    flag = 0;
+    if(i-minp<=maxp-i) {
+        if(i==point[curr])
+            p1=curr-1, p2=curr+1;
+        else
+            p1=curr, p2=curr+1;
+        while(p1>0) {
+            if(i-(float)point[p1] != (float)point[p2]-i) {
+                flag=1;
+                break;
+            }
+            p1--, p2++;
+        }
+    }
+    else {
+        if(i==point[curr])
+            p1=curr-1, p2=curr+1;
+        else
+            p1=curr, p2=curr+1;
+        while(p2<=n) {
+            if(i-(float)point[p1] != (float)point[p2]-i) {
+                flag=1;
+                break;
+            }
+            p1--, p2++;
+        }
+    }
+    return !flag;
+}
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n, l, i, minp, maxp, curr=1, ans=0, flag, p1, p2;
+    int t, l, ans=0, flag;
+    double i;
     cin>>n>>l;
-    for(i=1;i<=n;i++)
-        cin>>point[i];
+    for(t=1;t<=n;t++)
+        cin>>point[t];
     sort(point+1, point+n+1);
     minp=point[1], maxp=point[n];
-    for(i=minp+1;i<maxp;i++) {
-        flag = 0;
-        if(i==point[curr+1])
+    for(t=1;t<n;t++) {
+        if(t!=1) {
+            i=(float)point[t];
             curr++;
-        if(i-minp<=maxp-i) {
-            if(i==point[curr])
-                p1=curr-1, p2=curr+1;
-            else
-                p1=curr, p2=curr+1;
-            while(p1>0) {
-                if(i-point[p1] != point[p2]-i) {
-                    flag=1;
-                    break;
-                }
-                p1--, p2++;
-            }
+            if(fold(i))
+                ans++;
         }
-        else {
-            if(i==point[curr])
-                p1=curr-1, p2=curr+1;
-            else
-                p1=curr, p2=curr+1;
-            while(p2<=n) {
-                if(i-point[p1] != point[p2]-i) {
-                    flag=1;
-                    break;
-                }
-                p1--, p2++;
-            }
-        }
-        if(!flag)
+        i=((float)point[t]+(float)point[t+1])/2;
+        if(fold(i))
             ans++;
     }
     cout<<ans;
