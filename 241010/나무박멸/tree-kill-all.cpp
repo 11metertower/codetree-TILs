1123713c76a1e9dc84f0bb6b9fe0d4cda06dcffa
11metertower <87414569+11metertower@users.noreply.github.com>
2024-10-10 19:23:43 +0900

    Title: 나무박멸, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/나무박멸/tree-kill-all.cpp
+++ 241010/나무박멸/tree-kill-all.cpp
@@ -11,7 +11,7 @@ int loc2[4][2] = { {-1,1},{-1,-1},{1,-1},{1,1} };
 void grow(void)
 {
 	int i, j, t, x, y;
-	int tmp_mapp[21][21];
+	long tmp_mapp[21][21];
 	memset(tmp_mapp, 0, sizeof(tmp_mapp));
 	for (i = 1; i <= n; i++) {
 		for (j = 1; j <= n; j++) {
@@ -32,7 +32,7 @@ void grow(void)
 void divide(void)
 {
 	int i, j, t, cnt, x, y;
-	int tmp_mapp[21][21];
+	long tmp_mapp[21][21];
 	memset(tmp_mapp, 0, sizeof(tmp_mapp));
 	for (i = 1; i <= n; i++) {
 		for (j = 1; j <= n; j++) {
@@ -56,10 +56,16 @@ void divide(void)
 			mapp[i][j] += tmp_mapp[i][j];
 }
 
-int kill(void)
+long kill(void)
 {
 	int i, j, t, it, x, y, tx, ty;
 	long maxx = 0, cnt;
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			if (mapp[i][j] < 0)
+				mapp[i][j]++;
+		}
+	}
 	for (i = 1; i <= n; i++) {
 		for (j = 1; j <= n; j++) {
 			if (mapp[i][j] > 0) {
@@ -81,24 +87,23 @@ int kill(void)
 			}
 		}
 	}
-	i = tx, j = ty;
-	mapp[i][j] = -c;
-	for (t = 0; t < 4; t++) {
-		for (it = 1; it <= k; it++) {
-			x = i + loc2[t][0] * it, y = j + loc2[t][1] * it;
-			if (x >= 1 && x <= n && y >= 1 && y <= n) {
-				if (mapp[x][y] < -10)
-					break;
-				mapp[x][y] = -c;
-				if (mapp[x][y] == 0)
-					break;
+	if (maxx != 0) {
+		i = tx, j = ty;
+		mapp[i][j] = -c;
+		for (t = 0; t < 4; t++) {
+			for (it = 1; it <= k; it++) {
+				x = i + loc2[t][0] * it, y = j + loc2[t][1] * it;
+				if (x >= 1 && x <= n && y >= 1 && y <= n) {
+					int flag = 0;
+					if (mapp[x][y] < -10)
+						break;
+					if (mapp[x][y] <= 0)
+						flag = 1;
+					mapp[x][y] = -c;
+					if(flag)
+						break;
+				}
 			}
-		}
-	}
-	for (i = 1; i <= n; i++) {
-		for (j = 1; j <= n; j++) {
-			if (mapp[i][j] < 0 && mapp[i][j] != -c)
-				mapp[i][j]++;
 		}
 	}
 	return maxx;
